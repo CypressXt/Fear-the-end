3364770bc4c416d9bab77db60594e2b7c9e3fa2e
CypressXt <cypress@cypressxt.net>
2013-11-19 00:43:18 +0100

    Updated scaling properties

--- view/vIndex.php
+++ view/vIndex.php
@@ -8,7 +8,7 @@
         <script type="text/javascript" src="javascript/jquery.js"></script>
         <script type="text/javascript" src="javascript/popup.js"></script>
         <title>Fear the end</title>
-        <meta name="viewport" content="width=device-width, initial-scale=0.0, minimum-scale=0.8, maximum-scale=2.0" />
+<!--        <meta name="viewport" content="width=device-width, initial-scale=0.0, minimum-scale=0.8, maximum-scale=2.0" />-->
     </head>
     <body onload="initAll(document.getElementById('player'), document.getElementById('log'), document.getElementById('progress'))">
         <?php
